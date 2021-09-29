0a5b3f6115c24a3f6ccfd4b561fbc2392cf9e8f6
MemazoG <memogarcia246@gmail.com>
2021-09-29 00:21:25 -0500

    add functionality to add, check and delete buttons

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(".agregar").on("click", (e) => {
+    //Prevents page reload
+    e.preventDefault();
+    //Gets the text value from input field
+    const itemName = $("#newText").val();
+    //List element structure
+    let msg = 
+    `<li>
+        <div class="lis">
+            <p>${itemName}</p> <br>
+            <button class="checar">check</button>
+            <button class="del">delete</button>
+        </div>
+    </li>`
+    //Appends item to list
+    $(".Lista").append(msg)
+    //Clears input field
+    $("#newText").val("")
+})
+
+$(".Lista").on("click", "button", function(e){
+    //Prevents page reload
+    e.preventDefault();
+    //Gets reference to button
+    const btn = $(this);
+
+    if(btn.text() === "check"){             //'check' button
+        $(this).parent().toggleClass("chec")
+    } else if(btn.text() === "delete") {    //'delete' button
+        $(this).parent().remove()
+    }
+
+})
